520e6f02927554d4a6b5ef9a3bfad59fe6ed6336
Mason Green <masongreen@mason-greens-macbook-pro.local>
2009-04-29 18:36:32 -0400

    fixed crew/battery update

--- hud/HUD.hx
+++ hud/HUD.hx
@@ -59,7 +59,9 @@ class HUD extends Sprite
 	var s1 : flash.display.Shape;
 	var s1Special : flash.display.Shape;
 	var ship1Bat : Sprite;
+	var bat1Pos : Vector;
 	var ship1Crew : Sprite;
+	var crew1Pos : Vector;
 	var ship1Title : TextField;
 	var crewBat1 : TextField;
 	
@@ -67,9 +69,28 @@ class HUD extends Sprite
 	var ship2Icon : Bitmap;
 	var s2 : flash.display.Shape;
 	var ship2Bat : Sprite;
+	var bat2Pos : Vector;
 	var ship2Crew : Sprite;
+	var crew2Pos : Vector;
 	var ship2Title : TextField;
 	var crewBat2 : TextField;
+
+	var white : Int;
+	var green : Int;
+	var red : Int;
+	var limeGreen : Int;
+
+	public function new() {
+		super();
+		white = 0xFFFFFF;
+		green = 0x33CC11;
+		red = 0xFF0000;
+		limeGreen = 0x00FF00;
+		crew1Pos =  new Vector(515, 152);
+		bat1Pos =  new Vector(625, 152);
+		crew2Pos = new Vector(515, 402);
+		bat2Pos =  new Vector(625, 402);
+	}
 	
 	public function init(ship1Icon:Bitmap, ship2Icon:Bitmap) {
 		
@@ -79,11 +100,6 @@ class HUD extends Sprite
 		this.ship1Icon = ship1Icon;
 		this.ship2Icon = ship2Icon;
 
-		var white = 0xFFFFFF;
-		var green = 0x33CC11;
-		var red = 0xFF0000;
-		var limeGreen = 0x00FF00;
-		
 		// Ship 1
 		print(new Vector(505, 5), ship1.name, white);
 		print(new Vector(505, 125), ship1.captain, white);
@@ -96,10 +112,14 @@ class HUD extends Sprite
 		ship1Icon.scaleX = 2.25;
 	
 		ship1Crew = new Sprite();
-		initState(ship1Crew, ship1.crewCapacity, new Vector(515, 152), green);
+		var cap = ship1.crewCapacity;
+		updateState(ship1Crew, cap, cap, crew1Pos, green);
+		addChild(ship1Crew);
 		
 		ship1Bat = new Sprite();
-		initState(ship1Bat, ship1.batteryCapacity, new Vector(625, 152), red);
+		cap = ship1.batteryCapacity;
+		updateState(ship1Bat, cap, cap, bat1Pos, red);
+		addChild(ship1Bat);
 		
 		s1 = new flash.display.Shape();
 		s1.x = 575.0;
@@ -132,11 +152,15 @@ class HUD extends Sprite
 		ship2Icon.scaleX = 2.25;
 		
 		ship2Crew = new Sprite();
-		initState(ship2Crew, ship2.crewCapacity, new Vector(515, 402), green);
+		cap = ship2.crewCapacity;
+		updateState(ship2Crew, cap, cap, crew2Pos, green);
+		addChild(ship2Crew);
 		
 		ship2Bat = new Sprite();
-		initState(ship2Bat, ship2.batteryCapacity, new Vector(625, 402), red);
-		
+		cap = ship2.batteryCapacity;
+		updateState(ship2Bat, cap, cap, bat2Pos, red);
+		addChild(ship2Bat);
+
 		s2 = new flash.display.Shape();
 		s2.x = 575.0;
 		s2.y = 340.0;
@@ -149,41 +173,37 @@ class HUD extends Sprite
 	}
 	
 	public function loop() {
-		updateState(ship1Crew, ship1.crew);
-		updateState(ship1Bat, ship1.battery);
-		updateState(ship2Crew, ship2.crew);
-		updateState(ship2Bat, ship2.battery);
+		updateState(ship1Crew, ship1.crewCapacity, ship1.crew, crew1Pos, green);
+		updateState(ship1Bat, ship1.batteryCapacity, ship1.battery, bat1Pos, red);
+		updateState(ship2Crew, ship2.crewCapacity, ship2.crew, crew2Pos, green);
+		updateState(ship2Bat, ship2.batteryCapacity, ship2.battery, bat2Pos, red);
 		updateSpecial();
 	}
 	
-	function initState(sprite:Sprite, num:Int, pos:Vector, color:Int) {
+	function updateState(sprite:Sprite, max:Int, num:Int, pos:Vector, color:Int) {
+		var p = pos.clone();
 		var g = sprite.graphics;
+		g.clear();
 		g.lineStyle(1);
 		g.beginFill(0x000000, 1);
-		var height = (num / 2) * 5 + 2;
-		g.drawRect(pos.x, pos.y - height, 13, height);
-		pos.x += 2;
-		pos.y -= 4;
+		var height = (max / 2) * 5 + 2;
+		g.drawRect(p.x, p.y - height, 13, height);
+		p.x += 2;
+		p.y -= 4;
 		var c = 0;
-		for (i in 0...cast(num / 2, Int)) {
+		for (i in 0...Math.ceil(num/2)) {
 			c++;
-			var block = new flash.display.Shape();
-			g = block.graphics;
 			g.lineStyle(1.0, color);
 			g.beginFill(color, 1.0);
-			g.drawRect(pos.x, pos.y - 5 * i, 4, 2);
-			sprite.addChild(block);
-			if (c == ship1.crew) break;
-			block = new flash.display.Shape();
-			g = block.graphics;
+			g.drawRect(p.x, p.y - 5 * i, 4, 2);
+			if (c == num) break;
 			g.lineStyle(1.0, color);
 			g.beginFill(color, 1.0);
-			g.drawRect(pos.x + 6, pos.y - 5 * i, 4, 2);
-			sprite.addChild(block);
+			g.drawRect(p.x + 6, p.y - 5 * i, 4, 2);
 		}
-		addChild(sprite);
 	}
 	
+	/*
 	function updateState(sprite:Sprite, num:Int) {
 		var c1 = sprite.numChildren;
 		var delta = c1 - num;
@@ -195,6 +215,7 @@ class HUD extends Sprite
 			// TODO: add crew/battery back
 		}
 	}
+	*/
 	
 	function drawShape(g:Graphics, s:phx.Shape, color:Int) {
 		g.lineStyle(1.0, color);
